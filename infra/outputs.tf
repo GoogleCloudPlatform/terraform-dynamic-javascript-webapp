e8d8b5b5352d72b92cd8c8033c422c455e3e8552
Don McCasland <patatha@gmail.com>
2023-04-26 08:02:13 -0700

    fix: Update outputs.tf to stay at solutions deployments page

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -21,7 +21,7 @@ output "frontend_url" {
 
 output "neos_toc_url" {
   description = "Tutorial URL"
-  value       = "http://console.cloud.google.com?walkthrough_id=solutions-in-console--dynamic-javascript-webapp--dynamic-javascript-webapp_tour"
+  value       = "http://console.cloud.google.com/products/solutions/deployments?walkthrough_id=solutions-in-console--dynamic-javascript-webapp--dynamic-javascript-webapp_tour"
 }
 
 output "run_service_name" {

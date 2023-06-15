6da293033068328057542368b480c71ccebecc0c
Don McCasland <donmccasland@google.com>
2023-06-15 10:12:50 -0700

    add simple_example fixture

---
+++ infra/examples/simple_example/README.md
@@ -0,0 +1,27 @@
+# Simple Example
+
+This example illustrates how to use the `dynamic-python-webapp` module.
+
+<!-- BEGINNING OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|------|---------|:--------:|
+| project\_id | The ID of the project in which to provision resources. | `string` | n/a | yes |
+
+## Outputs
+
+| Name | Description |
+|------|-------------|
+| client\_job\_name | Client Cloud Run job name |
+| firebase\_url | Firebase URL |
+| server\_service\_name | Server Cloud Run service name |
+| usage | Connection details for the project |
+
+<!-- END OF PRE-COMMIT-TERRAFORM DOCS HOOK -->
+
+To provision this example, run the following from within this directory:
+- `terraform init` to get the plugins
+- `terraform plan` to see the infrastructure plan
+- `terraform apply` to apply the infrastructure build
+- `terraform destroy` to destroy the built infrastructure
---
+++ infra/examples/simple_example/outputs.tf
@@ -0,0 +1,26 @@
+/**
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+output "frontend_url" {
+  description = "IP address to site. Load balancer expected to take ~5 minutes for it to warm up."
+  value       = "http://${google_compute_global_address.default.address}/"
+}
+
+output "run_service_name" {
+  description = "The name of the deployed Cloud Run service."
+  value       = google_cloud_run_v2_service.default.name
+}
+

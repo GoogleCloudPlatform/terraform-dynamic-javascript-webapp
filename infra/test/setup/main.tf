826ede3e8efe5848e29200a8fa6bec009ec0dba4
rogerthatdev <roger.that.dev@gmail.com>
2023-07-12 12:38:30 -0700

    lint

--- infra/test/setup/main.tf
+++ infra/test/setup/main.tf
@@ -27,7 +27,7 @@ module "project" {
   activate_apis = [
     "cloudresourcemanager.googleapis.com",
     "storage-api.googleapis.com",
-    "serviceusage.googleapis.com", 
+    "serviceusage.googleapis.com",
     "cloudasset.googleapis.com"
   ]
 }

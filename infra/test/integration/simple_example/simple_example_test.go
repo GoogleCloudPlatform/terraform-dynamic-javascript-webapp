daae1e337a6461b62e43967b431992675345b4e4
rogerthatdev <roger.that.dev@gmail.com>
2023-07-24 11:27:18 -0700

    define verify for int tests

--- infra/test/integration/simple_example/simple_example_test.go
+++ infra/test/integration/simple_example/simple_example_test.go
@@ -15,11 +15,13 @@
 package multiple_buckets
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/GoogleCloudPlatform/cloud-foundation-toolkit/infra/blueprint-test/pkg/gcloud"
 	"github.com/GoogleCloudPlatform/cloud-foundation-toolkit/infra/blueprint-test/pkg/tft"
 	"github.com/GoogleCloudPlatform/cloud-foundation-toolkit/infra/blueprint-test/pkg/utils"
+	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -27,13 +29,29 @@ func TestSimpleExample(t *testing.T) {
 	example := tft.NewTFBlueprintTest(t)
 
 	example.DefineVerify(func(assert *assert.Assertions) {
-		example.DefaultVerify(assert)
+		// example.DefaultVerify(assert) // disables default verify
 
 		projectID := example.GetStringOutput("project_id")
-		services := gcloud.Run(t, "services list", gcloud.WithCommonArgs([]string{"--project", projectID, "--format", "json"})).Array()
+		server_service_name := terraform.OutputRequired(t, example.GetTFOptions(), "run_service_name")
 
-		match := utils.GetFirstMatchResult(t, services, "config.name", "storage.googleapis.com")
-		assert.Equal("ENABLED", match.Get("state").String(), "storage service should be enabled")
+		{
+			// Check that expected Cloud Run service is deployed
+			cloudRunServices := gcloud.Run(t, "run services list", gcloud.WithCommonArgs([]string{"--filter", "metadata.name=" + server_service_name, "--project", projectID, "--format", "json"})).Array()
+			nbServices := len(cloudRunServices)
+			assert.Equal(1, nbServices, "expected a single Cloud Run services called %s to be deployed, found %d services", server_service_name, nbServices)
+			match := utils.GetFirstMatchResult(t, cloudRunServices, "kind", "Service")
+			serviceURL := match.Get("status.url").String()
+			assert.Truef(strings.HasSuffix(serviceURL, ".run.app"), "unexpected service URL %q", serviceURL)
+			t.Log("Cloud Run service is running at", serviceURL)
+		}
+
+		{
+			// TODO: Check that load balancer endpoint returns 200
+		}
+
+		{
+			// TODO: Check Firestore accessible from frontend service
+		}
 	})
 	example.Test()
 }

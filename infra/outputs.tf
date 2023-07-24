ed3ca0e6b3ecaa3c0dc7f7254454bebefc2327ce
rogerthatdev <roger.that.dev@gmail.com>
2023-07-24 12:20:49 -0700

    update outputs

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -13,7 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
+output "project_id" {
+  description = "Project ID where solution is deployed."
+  value       = var.project_id
+}
 output "frontend_url" {
   description = "IP address to site. Load balancer expected to take ~5 minutes for it to warm up."
   value       = "http://${google_compute_global_address.default.address}/"

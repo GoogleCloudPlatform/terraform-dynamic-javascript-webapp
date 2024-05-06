bb72bf67d9f1c50ea262c611d3d8e00e098a8bed
Nim Jayawardena <i.am.nim.jay@gmail.com>
2024-05-06 09:07:23 -0400

    Fix terraform fmt issues

--- infra/main.tf
+++ infra/main.tf
@@ -332,9 +332,9 @@ data "google_cloud_asset_resources_search_all" "default_firestore_database" {
 # then skip this resource ("default" Firestore database).
 resource "google_firestore_database" "database" {
   count = (
-    var.init_firestore # Is this Terraform module being asked to create it?
-    && (!local.has_default_firestore_database # Don't create if the default Firestore DB exists...
-      || var.google_firestore_database.database[0]) # ... unless that default Firestore DB was created by this Terraform module.
+    var.init_firestore                            # Is this Terraform module being asked to create it?
+    && (!local.has_default_firestore_database     # Don't create if the default Firestore DB exists...
+    || var.google_firestore_database.database[0]) # ... unless that default Firestore DB was created by this Terraform module.
   ) ? 1 : 0
   project                     = var.project_id
   name                        = "(default)"
@@ -343,7 +343,7 @@ resource "google_firestore_database" "database" {
   concurrency_mode            = "PESSIMISTIC"
   app_engine_integration_mode = "DISABLED"
   # "ABANDON" means "terraform destroy" won't actually destroy this Google Cloud resource.
-  deletion_policy             = "ABANDON"
+  deletion_policy = "ABANDON"
   depends_on = [
     time_sleep.project_services
   ]
